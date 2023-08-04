6d5809174a1be466fd2ea9ef370b78348d56af6b
AsmitaTripathi <asmita.tripa2004thi@gmail.com>
2023-08-04 16:05:25 +0530

    Commit with first tutorial code

--- prac1.sh
+++ prac1.sh
@@ -2,7 +2,7 @@
 filename="employee.txt"
 i="1"
 while [[ $i -gt 0 ]];
-echo "Choose from the options below : \n 1. Add a record \n 2. Delete a record \n 3. Search a record \n 4. Sort the records \n 5. List all records \n 6. Exit"
+echo -e "Choose from the options below : \n 1. Add a record \n 2. Delete a record \n 3. Search a record \n 4. Sort the records \n 5. List all records \n 6. Exit"
 read -p "Choose from options (1/2/3/4/5) : " option
 
 do
@@ -26,7 +26,7 @@ do
 			read -p "Do you wanna sort in 1. EID 2. Ename " op
 			case $op in 
 				"1")
-					sort -k 1 $filename
+					sort -h -k 1 $filename
 				       	;;
 				"2") 
 					sort -k 2 $filename
